7cb6522e51b0fd226df1fe8fecd60e9805ca1ed7
Forsythia-olive <forgiveagbesi7478@gmail.com>
2022-12-08 12:31:51 +0000

    initial commit

--- src/OutputFormatting.java
+++ src/OutputFormatting.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class OutputFormatting {
+import java.util.Scanner;
+public class OutputFormatting {
+    static Scanner sc=new Scanner(System.in);
+        public static void main(String[] args) {
+
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x =sc.nextInt();
+                //Complete this line
+                System.out.printf("%-15s", s1);
+                System.out.printf("%03d %n", x);
+            }
+            System.out.println("================================");
+        }
 }
+
+
